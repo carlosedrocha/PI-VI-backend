53cc6d00f48b9d46e9e1302f69428eb43203545f
RAFAEL ZAGUI CAMARGO <rafael.zagui@puc-campinas.edu.br>
2023-09-13 08:15:54 -0300

    Colocando o cors e removendo a node_modules

--- app.js
+++ app.js
@@ -3,12 +3,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./src/routes/index');
-
+const cors = require('cors')
 const app = express();
 
 // Middleware para analisar solicitações JSON
 app.use(bodyParser.json());
-
+app.use(cors());
 // Rotas
 app.use('/', routes); // Prefixamos com '/user' para seguir boas práticas
 
