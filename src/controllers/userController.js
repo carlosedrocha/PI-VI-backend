38c21e899bdaae1f3f6b4deb0b891466c1ca644a
Pedro Zampieri <pedro.z@puccampinas.edu.br>
2023-10-03 22:36:15 -0300

    teste

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,96 +1,94 @@
-// controllers/user.controller.js
-
 const userModel = require('../models/userModel');
+const express = require('express')
 
-async function createUser(req, res) {
-  try {
-    const { username, email, password } = req.body;
-    const user = await userModel.createUser({ username, email, password });
-    res.status(201).json(user);
-  } catch(error) {
+class UserController {
+  constructor() {
+    this.router = express.Router();
+    // this.setupRoutes();
+  }
 
-    res.status(400).json({ error: 'Erro ao criar usuário' });
+  // setupRoutes() {
+  //   this.router.post('/create', this.createUser);
+  //   this.router.get('/all', this.selectUsers);
+  //   this.router.delete('/delete/:id', this.deleteUser);
+  //   this.router.put('/updateUserName/:id', this.updateUserName);
+  //   this.router.put('/updateEmail/:id', this.updateEmail);
+  //   this.router.put('/updatePassword/:id', this.updatePassword);
+  //   this.router.post('/login', this.login);
+  // }
+
+  async createUser(req, res) {
+    try {
+      const { username, email, password } = req.body;
+      const user = await userModel.createUser({ username, email, password });
+      res.status(201).json(user);
+    } catch (error) {
+      res.status(400).json({ error: 'Erro ao criar usuário' });
+    }
+  }
+
+  async selectUsers(req, res) {
+    try {
+      const users = await userModel.selectUsers();
+      res.json(users);
+    } catch (error) {
+      console.log(error)
+      res.status(400).json({ error: error });
+    }
+  }
+
+  async deleteUser(req, res) {
+    try {
+      const id = req.params.id;
+      const user = await userModel.deleteUser(id);
+      res.json(user);
+    } catch (error) {
+      res.status(400).json({ error: 'Erro interno do servidor' });
+    }
+  }
+
+  async updateUserName(req, res) {
+    try {
+      const id = req.params.id;
+      const { username } = req.body;
+      const user = await userModel.updateUserName(id, username);
+      res.json(user);
+    } catch (error) {
+      res.status(400).json({ error: 'Erro interno do servidor' }); 
+    }
+  }
+
+  async updateEmail(req, res) {
+    try {
+      const id = req.params.id;
+      const { email } = req.body;
+      const user = await userModel.updateEmail(id, email);
+      res.json(user);
+    } catch (error) {
+      res.status(400).json({ error: 'Erro interno do servidor' });
+    }
+  }
+
+  async updatePassword(req, res) {
+    try {
+      const id = req.params.id;
+      const { password } = req.body;
+      const user = await userModel.updatePassword(id, password);
+      res.json(user);
+    } catch (error) {
+      res.status(400).json({ error: 'Erro interno do servidor' });
+    }
+  }
+
+  async login(req, res) {
+    try {
+      const { username, password } = req.body;
+      const user = await userModel.login(username, password);
+      res.json(user);
+    } catch (error) {
+      res.status(400).json({ error: 'Erro interno do servidor' });
+    }
   }
 }
 
-async function selectUser(req, res){
-  try {
-    const users = await userModel.selectUsers();
-    res.json(users);
-  }
-  catch(error) {
-    
-    res.status(400).json({ error: 'Erro interno do servidor' });
-  }
-}
-
-async function deleteUser(req, res){
-  try {
-    const id = req.params.id;
-    const user = await userModel.deleteUser(id);
-    res.json(user);
-  }
-  catch(error) {
-    
-    res.status(400).json({ error: 'Erro interno do servidor' });
-  }
-}
-
-async function updateUserName(req, res){
-  try {
-    const id = req.params.id;
-    const { username } = req.body;
-    const user = await userModel.updateUserName(id, username);
-    res.json(user);
-  }
-  catch(error) {
-    
-    res.status(400).json({ error: 'Erro interno do servidor' });
-  }
-}
-
-async function updateEmail(req, res){
-  try {
-    const id = req.params.id;
-    const { email } = req.body;
-    const user = await userModel.updateEmail(id, email);
-    res.json(user);
-  }
-  catch(error) {
-    
-    res.status(400).json({ error: 'Erro interno do servidor' });
-  }
-}
-
-async function updatePassword(req, res){
-  try {
-    const id = req.params.id;
-    const { password } = req.body;
-    const user = await userModel.updatePassword(id,password);
-    res.json(user);
-  }
-  catch(error) {
-    res.status(400).json({ error: 'Erro interno do servidor' });
-  }
-}
-
-async function login(req, res){
-  try {
-    const { username, password } = req.body;
-    const user = await userModel.login(username, password);
-    res.json(user);
-  }
-  catch(error) {
-    res.status(400).json({ error: 'Erro interno do servidor' });
-  }
-}
-
-module.exports = {
-  createUser,
-  selectUser,
-  deleteUser,
-  updateUserName,
-  updateEmail,
-  updatePassword,
-  login
-};
+module.exports = UserController;
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,19 +2,26 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
-const userRoutes = router.post('/create', userController.createUser);
-const selectRoutes = router.get('/all', userController.selectUser);
-const deleteRoutes = router.delete('/delete/:id', userController.deleteUser);
-const updateUserNameRoutes = router.put('/updateUserName/:id', userController.updateUserName);
-const updateEmailRoutes = router.put('/updateEmail/:id', userController.updateEmail);
-const updatePasswordRoutes = router.put('/updatePassword/:id', userController.updatePassword);
-const loginRoutes = router.post('/login', userController.login);
+class UserRouter {
+  constructor() {
+    this.router = router;
+    this.userController = userController;
+    this.setupRoutes();
+  }
 
+  setupRoutes() {
+    this.router.get('/all', this.userController.selectUser);
+    // this.router.post('/create', this.userController.createUser);
+    // this.router.delete('/delete/:id', this.userController.deleteUser);
+    // this.router.put('/updateUserName/:id', this.userController.updateUserName);
+    // this.router.put('/updateEmail/:id', this.userController.updateEmail);
+    // this.router.put('/updatePassword/:id', this.userController.updatePassword);
+    // this.router.post('/login', this.userController.login);
+  }
 
-module.exports = userRoutes;
-module.exports = selectRoutes;
-module.exports = deleteRoutes;
-module.exports = updateUserNameRoutes;
-module.exports = updateEmailRoutes;
-module.exports = updatePasswordRoutes;
-module.exports = loginRoutes;
+  getRoutes() {
+    return this.router;
+  }
+}
+
+module.exports = new UserRouter().getRoutes();
