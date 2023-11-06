b026bccbc8b8152c6db0140fc2f9903571dbf55c
ANDREZ FERREIRA LUCIETTO <andrezlucietto@puc-campinas.edu.br>
2023-11-06 12:00:15 -0300

    Inicio da implementação do sockets

--- config.js
+++ config.js
@@ -5,4 +5,21 @@ module.exports = {
   // mongoURI: process.env.MONGODB_URI || 'mongodb://localhost/seu-banco-de-dados',
   jwtSecret: process.env.JWT_SECRET || 'segredo-super-seguro',
   // Outras configurações aqui...
+  async headers() {
+    return [
+      {
+        key: 'Access-Control-Allow-Origin',
+        value: '*',
+      },
+      {
+        key: 'Access-Control-Allow-Methods',
+        value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+      },
+      {
+        key: 'Access-Control-Allow-Headers',
+        value:
+          'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+      },
+    ];
+  }
 };
